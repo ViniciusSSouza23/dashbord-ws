757dbead98a0270e5adb1910944538ad1e6bc42e
Vinicius Souza <vinicius.souza@nre.tec.br>
2023-04-03 14:57:36 -0300

    add bootstrap and add footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="footer-component bg-dark">
+    <div class="container py-5">
+      <h6 class="text-center text-white">
+        Develop by
+        <a
+          href="https://github.com/ViniciusSSouza23"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Vinicius Souza</a
+        >
+      </h6>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer-component {
+  a {
+    text-decoration: none;
+    color: #fff;
+  }
+}
+</style>
